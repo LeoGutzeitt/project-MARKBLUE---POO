22c83da48980a22ddbdffbdd32560bec59884e58
gabrielvictalino <gabrielvictalino@gmail.com>
2025-10-11 18:16:50 -0300

    implementacao das etapas 2 e 3

--- src/br/edu/cs/poo/ac/ordem/daos/DAOGenerico.java
+++ src/br/edu/cs/poo/ac/ordem/daos/DAOGenerico.java
@@ -2,11 +2,8 @@ package br.edu.cs.poo.ac.ordem.daos;
 
 import br.edu.cesarschool.next.oo.persistenciaobjetos.CadastroObjetos;
 
-import br.edu.cs.poo.ac.ordem.entidades.CadastroObjetos;
-
 public class DAOGenerico {
-
-    protected br.edu.cs.poo.ac.ordem.daos.CadastroObjetos cadastroObjetos;
+    protected CadastroObjetos cadastroObjetos;
     public DAOGenerico(Class classe) {
         cadastroObjetos = new CadastroObjetos(classe);
     }
